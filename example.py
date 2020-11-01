6d7dff6163968931039f3fca07231297f0750991
Adam Schoenfeld <adamrschoenfeld311@gmail.com>
2020-11-01 01:10:12 -0500

    Updates

--- example.py
+++ example.py
@@ -1,24 +1,26 @@
 from EasyTuya import TuyaAPI
 from EasyTuya.devices import Lights
 
+# A simple program to control a group of lights through command line input
+
 if __name__ == "__main__":
-    api = TuyaAPI("9ea9sk54a0k2978837d6", "d6034d97286c4b049ee16874a5a2d92d")
-    l1 = Lights.Light("64304636a4cf12d76aad", "Light 1")
-    l2 = Lights.Light("55008855483fdac28931", "Light 2")
-    api.addDevices([l1, l2], "BRLIGHTS")
+    api = TuyaAPI("your_client_id", "your_access_secret")
+    l1 = Lights.Light("your_device_id_1", "Light 1")
+    l2 = Lights.Light("your_device_id_2", "Light 2")
+    api.addDevices([l1, l2], "LIGHTS")
     while(True):
         toDo = input()
         if toDo == "on":
-            api.sendGroupCommand("BRLIGHTS", Lights.onCommand())
+            api.sendGroupCommand("LIGHTS", Lights.onCommand())
         elif toDo == "off":
-            api.sendGroupCommand("BRLIGHTS", Lights.offCommand())
+            api.sendGroupCommand("LIGHTS", Lights.offCommand())
         elif toDo == "white":
-            api.sendGroupCommand("BRLIGHTS", Lights.colorCommand("white"))
+            api.sendGroupCommand("LIGHTS", Lights.colorCommand("white"))
         elif toDo == "rainbow":
-            api.sendGroupCommand("BRLIGHTS", Lights.gorgCommand(255, 191))
+            api.sendGroupCommand("LIGHTS", Lights.gorgCommand(255, 191))
         elif toDo.split()[0] == "bright":
-            api.sendGroupCommand("BRLIGHTS", Lights.brightCommand(int(toDo.split()[1])))
+            api.sendGroupCommand("LIGHTS", Lights.brightCommand(int(toDo.split()[1])))
         elif toDo == "onoff":
-            api.sendGroupCommand("BRLIGHTS", api.devices['BRLIGHTS'][0].toggleOnOff())
+            api.sendGroupCommand("LIGHTS", api.devices['BRLIGHTS'][0].toggleOnOff())
         elif toDo == "refresh":
             api.refreshAccessToken()
