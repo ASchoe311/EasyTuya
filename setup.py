e05b4197ec523dbd18b11447b18f0536041eb562
Adam Schoenfeld <adamrschoenfeld311@gmail.com>
2021-05-26 18:31:14 -0400

    Change Setup
    Removed unnecessary code from setup.py

--- setup.py
+++ setup.py
@@ -1,119 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Note: To use the 'upload' functionality of this file, you must:
-#   $ pipenv install twine --dev
+import pathlib
+from setuptools import setup
 
-import io
-import os
-import sys
-from shutil import rmtree
+here = pathlib.Path(__file__).parent.resolve()
 
-from setuptools import find_packages, setup, Command
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
-# Package meta-data.
-NAME = 'EasyTuya'
-DESCRIPTION = 'Interact with devices connected to the Tuya IOT platform through Python'
-URL = 'https://github.com/ASchoe311/EasyTuya'
-EMAIL = 'aschoe@umich.edu'
-AUTHOR = 'Adam Schoenfeld'
-REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.371'
-
-# What packages are required for this module to be executed?
-REQUIRED = [
-    'pycryptodome'
-]
-
-# What packages are optional?
-EXTRAS = {
-    # 'fancy feature': ['django'],
-}
-
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
-        exec(f.read(), about)
-else:
-    about['__version__'] = VERSION
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel'.format(sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
-
-        # self.status('Pushing git')
-        # os.system('git stage -A')
-        # os.system('git commit -m \"New PyPi release\"')
-        # os.system('git push')
-
-        sys.exit()
-
-
-# Where the magic happens:
 setup(
-    name=NAME,
-    version=about['__version__'],
-    description=DESCRIPTION,
+    name='EasyTuya',
+    version='0.1.371',
+    description='Interact with devices connected to the Tuya IOT platform through Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-	#download_url = "https://github.com/ASchoe311/TuyaPy/archive/0.1.tar.gz",
-    #packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    # If your package is a single module, use this instead of 'packages':
+    author='Adam Schoenfeld',
+    author_email='aschoe@umich.edu',
+    python_requires='>=3.6.0',
+    project_urls={
+        'Documentation': 'https://aschoe311.github.io/EasyTuya/',
+        'Source': 'https://github.com/ASchoe311/EasyTuya'
+    },
+    keywords='tuya iot tuyaapi smartlights',
     packages=['EasyTuya', 'EasyTuya.devices'],
-
     # entry_points={
     #     'console_scripts': ['mycli=mymodule:cli'],
     # },
-    install_requires=REQUIRED,
-    extras_require=EXTRAS,
+    install_requires=[
+        'pycryptodome'
+    ],
     include_package_data=True,
     license='GNU GPLv3',
     classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
@@ -121,9 +40,5 @@ setup(
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
 		'Development Status :: 1 - Planning'
-    ],
-    # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
+    ]
 )
